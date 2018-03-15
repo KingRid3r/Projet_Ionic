4c085b44d69ba760077235350f89cca40ecf93f2
BENEZIT Luca <luca.benezit@etu.univ-amu.fr>
2018-03-15 09:52:44 +0100

    ConnectionForm

--- src/pages/connection/connection.ts
+++ src/pages/connection/connection.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ViewController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import { Http } from '@angular/http';
 /**
  * Generated class for the ConnectionPage page.
  *
@@ -14,8 +16,9 @@ import { ViewController } from 'ionic-angular';
   templateUrl: 'connection.html',
 })
 export class ConnectionPage {
+	rep: any;
+  constructor(public http: Http, public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams) {
 
-  constructor( public viewCtrl: ViewController, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
@@ -23,6 +26,11 @@ export class ConnectionPage {
   }
   connect(){
   	console.log("test")
+  	this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?connexion&login=classe1&mdp=mdp1')
+ 		.map(res => res.json())
+ 		.subscribe(data => {
+	this.rep = data;
+	});
   }
   fermeture() {
  	this.viewCtrl.dismiss();
